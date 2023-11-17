56ae88f5ddf0883752d7f1b5a21963cfd02fd2f4
Lucas Lopes <lucaslopesdemelosilva@gmail.com>
2023-11-17 16:25:15 -0300

    Desenhando tela de Login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.instagram"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.instagram"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
